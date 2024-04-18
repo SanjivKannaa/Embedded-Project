80288b3b35d2cfd3619df141e858c20ce937098d
Sanjiv Kannaa Jeganathan <jsanjiv2003@gmail.com>
2024-04-18 23:55:11 +0530

    added static dashboard

--- server.py
+++ server.py
@@ -36,6 +36,10 @@ app =  Flask(__name__)
 def home():
     return render_template("home.html")
 
+@app.get("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
 @app.get("/api/safe")
 def safe_handler():
     try:
